571e1521c9155fafe565bd2527a7fff104b7997e
vietphung <harryportervn1993@gmail.com>
2025-10-16 16:08:49 +0200

    Add RepositoryInterface and ServiceInterface for core functionality

---
+++ backend/core/interfaces/RepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Core\Interfaces;
+
+interface RepositoryInterface
+{
+    public function findAll(): array;
+    public function findById(int $id): ?array;
+    public function create(array $data): int;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
+}
---
+++ backend/core/interfaces/ServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Core\Interfaces;
+
+interface ServiceInterface
+{
+    public function getAll(): array;
+    public function getById(int $id): ?array;
+    public function create(array $data): int;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
+}
